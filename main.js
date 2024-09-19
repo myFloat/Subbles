5dea71c522de4527a587e351e96c691b01515e39
myFloat <60947799+myFloat@users.noreply.github.com>
2024-09-19 20:30:12 +0200

    Initiate save format conversion

--- main.js
+++ main.js
@@ -6,10 +6,6 @@
 //	Button for touchscreen
 //General do for all children method (maybe)
 
-function compile() {
-	const string = "This function is no longer needed before saving."; 
-	print(string);
-}
 let toStorage;
 function prepareSave() {
     let content = [];
